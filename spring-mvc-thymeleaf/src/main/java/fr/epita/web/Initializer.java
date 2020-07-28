bf3fee83ec0f9b4c3e56aae501a0d50040714341
thomas <thomas.broussard@local.local>
2020-07-28 10:18:17 +0200

    * main controller + view

---
+++ spring-mvc-thymeleaf/src/main/java/fr/epita/web/Initializer.java
@@ -0,0 +1,23 @@
+package fr.epita.web;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Initializer extends
+        AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+    
+}
